d30c99170b2a82fcf7bfee2e14c5be237985be2f
Rameshwar <rameshwar.swami@mitwpu.edu.in>
2026-06-02 18:55:12 +0530

    Solve leetcode 242, 268 and 1929

---
+++ Arrays/242 Valid anagram.cpp
@@ -0,0 +1,18 @@
+/* 242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise. */
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s == t){
+            return true;
+        }
+        else {
+           return false;
+        }
+    }
+};
---
+++ Arrays/268_Missing number.cpp
@@ -0,0 +1,18 @@
+/* 268 missing-number 
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.*/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+    
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<n; i++){
+            if (nums[i] != i) {
+                return i;
+            }
+        } 
+        return n;
+    }
+};
