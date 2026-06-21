586d6d93b0bb5c851f58f9905ae63e1ad126fc3f
Rameshwar <rameshwar.swami@mitwpu.edu.in>
2026-06-21 11:49:33 +0530

    Solve leetcode 35 and 704

---
+++ Binary Search/704_Binary search.cpp
@@ -0,0 +1,29 @@
+/* 704 binary search 
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+};
